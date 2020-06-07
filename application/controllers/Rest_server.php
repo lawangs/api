9770254f2571a1cc81d6db67f99b8ed0fa56895e
Faisal Efendi <f.e@users.noreply.github.com>
2020-06-07 18:58:51 +0800

    Initial commit

---
+++ application/controllers/Rest_server.php
@@ -0,0 +1,12 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Rest_server extends CI_Controller
+{
+
+    public function index()
+    {
+        $this->load->view('rest_server');
+    }
+}
